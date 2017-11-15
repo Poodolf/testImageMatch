430bf8d126113acae7fa3ebfbd3a2185a614f86e
Poodolf <32989362+Poodolf@users.noreply.github.com>
2017-11-15 01:10:38 +0100

    code structure improvements

--- main.cpp
+++ main.cpp
@@ -1,16 +1,79 @@
-#include "match.h"
 #include <QDebug>
-#include <iostream>
+#include <QVector>
+#include <QRect>
+//#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
 
+std::string image_window = "Source Image";
+std::string result_window = "Result window";
+
+class Match{
+    public:
+        QRect rect;
+        double dMatch;
+};
+
+QVector<Match> matching(cv::Mat img, cv::Mat templ, int matchToFind)
+{
+    cv::namedWindow( image_window, CV_WINDOW_AUTOSIZE );
+    cv::namedWindow( result_window, CV_WINDOW_AUTOSIZE );
+
+    cv::Mat result;
+    cv::Mat img_display;
+    QVector<Match> hits;
+    img.copyTo( img_display );
+
+    int result_cols =  img.cols - templ.cols + 1;
+    int result_rows = img.rows - templ.rows + 1;
+    result.create( result_rows, result_cols, CV_32FC1 );
+    matchTemplate( img, templ, result, 5);
+    double matchingGrade;
+    int counter = 0; //Anzahl der gesuchten Treffer
+    while (true){
+
+        double minVal; double maxVal; cv::Point minLoc; cv::Point maxLoc; cv::Point matchLoc;
+        minMaxLoc( result, &minVal, &maxVal, &minLoc, &maxLoc, cv::Mat() );
+        matchLoc = maxLoc;
+        matchingGrade = maxVal;
+
+        if((maxVal > 0.80) && !(counter == matchToFind)){
+            ++counter;
+            Match newMatch;
+            QRect temp = {matchLoc.x, matchLoc.y, templ.cols, templ.rows};
+            newMatch.rect = temp;
+            newMatch.dMatch = maxVal;
+            hits.append(newMatch);
+
+            qDebug() << "Matching Number:" << counter << "," << matchingGrade << "\n" << newMatch.rect.left() << newMatch.rect.top() << newMatch.dMatch;
+            floodFill(result, cv::Point(matchLoc.x,matchLoc.y), cv::Scalar(0,0,0), 0, cv::Scalar(10,10,10));
+            rectangle( img_display, matchLoc, cv::Point( matchLoc.x + templ.cols , matchLoc.y + templ.rows ), cv::Scalar::all(0), 2, 8, 0 );
+            imshow( image_window, img_display );
+            imshow( result_window, result );
+            cv::waitKey(0); //Check Match by Match
+        }
+        else
+        {
+            if (counter == matchToFind){
+                qDebug() << counter << "matches found, exit searching";
+                break;
+            }
+            else {
+            qDebug() << "no more matches";
+            break;
+            }
+        }
+    }
+    return hits;
+}
 
 int main(int, char**)
 {
-
-    Mat img = imread("X://2.jpg", 1);
-    Mat templ = imread("X://2template.jpg", 1);
+    cv::Mat img = cv::imread("X://2.jpg", 1);
+    cv::Mat templ = cv::imread("X://2template.jpg", 1);
     int matchToFind = 10;
     QVector<Match> hits = matching(img, templ, matchToFind);
-    for(unsigned i = 0; i <= (hits.length() - 1); ++i){
+    for(int i = 0; i <= (hits.length() - 1); ++i){
     qDebug() << "hit " << (i + 1) << hits[i].rect.left() << hits[i].rect.top() <<hits[i].dMatch;
     }
     return 0;
--- testImageMatch.pro
+++ testImageMatch.pro
@@ -25,28 +25,29 @@ DEFINES += QT_DEPRECATED_WARNINGS
 
 SOURCES += main.cpp
 
-HEADERS  += \
-    match.h
+HEADERS  +=
 
-INCLUDEPATH += X:\opencv\build\include
+win32:INCLUDEPATH += X:\opencv\build\include
 
-LIBS += X:\opencv-build\bin\libopencv_core331.dll
-LIBS += X:\opencv-build\bin\libopencv_highgui331.dll
-LIBS += X:\opencv-build\bin\libopencv_imgcodecs331.dll
-LIBS += X:\opencv-build\bin\libopencv_imgproc331.dll
-LIBS += X:\opencv-build\bin\libopencv_features2d331.dll
-LIBS += X:\opencv-build\bin\libopencv_calib3d331.dll
+win32:LIBS += X:\opencv-build\bin\libopencv_core331.dll
+win32:LIBS += X:\opencv-build\bin\libopencv_highgui331.dll
+win32:LIBS += X:\opencv-build\bin\libopencv_imgcodecs331.dll
+win32:LIBS += X:\opencv-build\bin\libopencv_imgproc331.dll
+win32:LIBS += X:\opencv-build\bin\libopencv_features2d331.dll
+win32:LIBS += X:\opencv-build\bin\libopencv_calib3d331.dll
 
 # more correct variant, how set includepath and libs for mingw
 # add system variable: OPENCV_SDK_DIR=D:/opencv/build
 # read http://doc.qt.io/qt-5/qmake-variable-reference.html#libs
 
+unix:LIBS += -L/usr/local/lib -lmath
+
 #INCLUDEPATH += $$(OPENCV_SDK_DIR)/include
 
 #LIBS += -L$$(OPENCV_SDK_DIR)/x86/mingw/lib \
-#        -lopencv_core320        \
-#        -lopencv_highgui320     \
-#        -lopencv_imgcodecs320   \
-#        -lopencv_imgproc320     \
-#        -lopencv_features2d320  \
-#        -lopencv_calib3d320
+#        -lopencv_core331        \
+#        -lopencv_highgui331     \
+#        -lopencv_imgcodecs331   \
+#        -lopencv_imgproc331     \
+#        -lopencv_features2d331  \
+#        -lopencv_calib3d331
